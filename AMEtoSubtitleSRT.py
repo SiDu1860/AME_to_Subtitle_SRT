64c7f6ea38dbd7f013b111b8de33f7bca5d826a1
Simon <36437904+SiDu1860@users.noreply.github.com>
2021-02-26 12:03:23 +0100

    Update AMEtoSubtitleSRT.py

--- AMEtoSubtitleSRT.py
+++ AMEtoSubtitleSRT.py
@@ -1,8 +1,7 @@
+# Version: 0.5.1 beta
+# Last changes: 2021-02-26
 
-# created by Simon Duschl, ARRI, sduschl@arri.de | Arnold & Richter Cinetechnik, Munich (c) 2021
-# Read CSV from ARRI Meta Extract and write the values from a given column frame accurate into a SRT Subtitle File.
-# e.g. frame accurate Focus Distance information. The SRT can be used in many post production tools.
-# external module "timecode" is used -> https://pypi.org/project/timecode/#description
+
 
 from pathlib import Path
 from timecode import Timecode
